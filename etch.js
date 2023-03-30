1481dc1d3c6f991ecd263266bbe97d24fcdd88c6
Jeremiah <jeremiahjacobson10@gmail.com>
2023-03-30 15:43:44 -0400

    completed rough draft of program

--- etch.js
+++ etch.js
@@ -1,1 +1,26 @@
-console.log("test");
+function changeColour(e) {
+  e.target.style.backgroundColor = "blue";
+  //   console.log(e.target);
+}
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+  square.draggable = false;
+  square.addEventListener("mouseover", changeColour);
+});
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     alert(button.id);
+//   });
+// });
+
+function resetSquares(e) {
+  squares.forEach((square) => {
+    square.style.backgroundColor = "bisque";
+  });
+}
+
+const resetBtn = document.querySelector(".resetBtn");
+
+resetBtn.addEventListener("click", resetSquares);
